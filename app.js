ccbda7cd87e06ba8e61872d84fcf5a9b1a3830d8
Dueon Kim <dueonkim89@Yahoo.com>
2018-10-28 14:49:26 -0700

    create get route handler for retrieving block

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 
+const { Blockchain } = require('./simpleChain');
+const { Block } = require('./simpleBlock');
+
 const app = express();
 const port = process.env.PORT || 8000;
+const newBlockChain = new Blockchain();
 
 app.use(bodyParser.json());
+
+app.get("/block/:height", (req, res) => {
+	const height = req.params.height;
+	newBlockChain.getBlock(height).then( block => {	
+		res.send(block);
+	}).catch(error => res.status(400).send(error));				
+});
+
 
 app.listen(port, () => {
 	console.log(`listening on port ${port}`);
